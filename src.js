331b1efd82fde8dcb252900d8665c6a5e8802b86
Vitor Oliveira <vyctoroliveira28@gmail.com>
2022-09-25 11:10:24 -0300

    tamo indo

---
+++ src.js
@@ -0,0 +1,7 @@
+let nome = document.getElementById('nome')
+let senha = document.getElementById('senha')
+let enter = document.getElementById('enter')
+
+function enter(''){
+    window.alert('ola')
+}
